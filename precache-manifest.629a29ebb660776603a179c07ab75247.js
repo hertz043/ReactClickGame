ced71771100cecb92a15866cc13be243e14d2871
Hertz <43124833+hertz043@users.noreply.github.com>
2018-11-20 10:06:45 -0600

    Updates

---
+++ precache-manifest.629a29ebb660776603a179c07ab75247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "02f90760c28f3fec8bc6",
+    "url": "/ReactClickGame/static/js/runtime~main.02f90760.js"
+  },
+  {
+    "revision": "60a118fcef5f395137ab",
+    "url": "/ReactClickGame/static/js/main.60a118fc.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/ReactClickGame/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "60a118fcef5f395137ab",
+    "url": "/ReactClickGame/static/css/main.5e484fc2.chunk.css"
+  },
+  {
+    "revision": "b3facba677ee8de03b63fbeb24d5db3f",
+    "url": "/ReactClickGame/index.html"
+  }
+];
